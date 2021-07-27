6f1a0240dcd08e3fefd90d03db6cc821231696ed
DESKTOP-KL0TM1V\xt18 <tonghao-company>
2021-07-27 09:29:22 +0800

    fix:解决冲突

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,25 +4,6 @@ import c from 'classnames'
 import Head from '../head'
 import Body from '../body'
 import Bottom from '../bottom'
-<<<<<<< HEAD
-
-
-const Home = () => {
-  return (
-    <div className={c('wh100p fbv', s.body)}>
-			<div>
-				<Head />
-			</div>
-
-			<div className={c('fbh')}>
-				<div>左侧菜单</div>
-				<div>右侧菜单</div>
-			</div>
-
-			<div>
-				<Bottom />
-			</div>
-=======
 // import globalEvent from '../../utils/global-event'
 
 const Home = () => {
@@ -40,7 +21,6 @@ const Home = () => {
       <div>
         <Bottom />
       </div>
->>>>>>> c7d71a7163a4256237e967c435193e56e0875394
     </div>
   )
 }
