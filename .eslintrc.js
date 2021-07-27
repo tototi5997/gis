dd742aad8783e3ef8797adce77261d8d3497117a
DESKTOP-KL0TM1V\xt18 <tonghao-company>
2021-07-27 09:21:43 +0800

    fix: 更改框架结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
     ],
     "rules": {
         "react/prop-types": 0,
-        "no-unused-vars":["error"],
+        "no-unused-vars":["off"],
     }
 };
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,31 +3,24 @@ import s from './home.module.styl'
 import c from 'classnames'
 import Head from '../head'
 import Bottom from '../bottom'
-import globalEvent from '../../utils/global-event'
+
 
 const Home = () => {
-    useEffect(() => {
-        // 接受监控的广播信息
-        globalEvent.on('MBottom.Test', ({data}) => {
-            console.log('event data', data)
-        })
-    })
-
-    return (
+  return (
     <div className={c('wh100p fbv', s.body)}>
-        <div>
-            <Head />
-        </div>
+			<div>
+				<Head />
+			</div>
 
-        <div className={c('fbh')}>
-            <div>左侧菜单</div>
-            <div>右侧菜单</div>
-        </div>
+			<div className={c('fbh')}>
+				<div>左侧菜单</div>
+				<div>右侧菜单</div>
+			</div>
 
-        <div>
-            <Bottom />
-        </div>
+			<div>
+				<Bottom />
+			</div>
     </div>
-    )
+  )
 }
 export default Home
